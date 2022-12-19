3c291ae09f44bff779ef91ebae0f156e5c61f9d3
Ruben Garcia <rubengarciahernandez@gmail.com>
2022-12-19 16:29:55 +0100

    Tesis dpto

---
+++ docs/tesisDpto/IngSoft.C
@@ -0,0 +1,8 @@
+doctor JoseParets: director GironIrueste,   director JuanCarlosTorres{};
+doctor MaJoseRodriguez: director JoseParets{};
+doctor LinaGarcia: director JoseParets, director MaJoseRodriguez{};
+doctor JuanJesusTorres: director JoseParets{};
+doctor MaviHurtado: director JoseParets, director LinaGarcia{};
+doctor PatriciaPaderewski : director JoseParets, director MaJoseRodriguez{};
+doctor NuriaMedina: director JoseParets, director LinaGarcia{};
+
---
+++ docs/tesisDpto/tesis.C
@@ -0,0 +1,57 @@
+#include "tesis.H"
+
+doctor MiguelDelgado externo
+doctor XavierPueyo externo
+doctor GironIrueste externo
+doctor DomingoGimenez externo
+doctor TroyaLinero externo
+doctor ManuelCollado externo
+doctor FelixSaltor externo
+doctor FranciscoHerrera externo
+doctor OscarCordon externo
+doctor LuisHernandez externo
+doctor YannisManolopoulos externo
+doctor RafaelMolina externo
+doctor AKKatsaggelos externo
+doctor ManuelLozano externo
+doctor JulioOrtega externo
+doctor JoseACarrillo externo
+
+doctor BuenaventuraClares: director MiguelDelgado{};
+doctor RafaelMorales: director BuenaventuraClares{};
+doctor JuanCarlosTorres : director BuenaventuraClares{};
+doctor ManuelCapel: director  TroyaLinero{};
+doctor JuanCarlosGranja: director ManuelCollado{};
+doctor FranciscoFeito:  director JuanCarlosTorres{};
+doctor ManuelJoseBarranco: director JuanCarlosGranja{};
+doctor IsidroVerdu:  director JuanCarlosTorres, director DomingoGimenez{};
+doctor MiguelGea:  director JuanCarlosTorres{};
+doctor JoseSamos: director FelixSaltor {};
+doctor CarlosUrena : director JuanCarlosTorres, director XavierPueyo {};
+doctor DomingoMartin:  director JuanCarlosTorres{};
+doctor PedroVillar: director FranciscoHerrera, director OscarCordon{};
+doctor JorgeRevelles: director CarlosUrena {};
+doctor MaMarAbad: director LuisHernandez{};
+doctor NicolasPadilla: director MiguelGea{};
+doctor ManuelTorres: director JoseSamos{};
+doctor AlbertoAbello: director FelixSaltor, director JoseSamos{};
+doctor AntonioCorral: director YannisManolopoulos, director JoseSamos{};
+doctor AntonioLopez: director RafaelMolina, director AKKatsaggelos{};
+doctor FranciscoLuisGutierrez: director JuanCarlosTorres, director MiguelGea{};
+doctor AnaMaSanchezLopez: director FranciscoHerrera, director ManuelLozano{};
+doctor MiguelHornos: director ManuelCapel{};
+doctor FranciscoVelasco: director JuanCarlosTorres{};
+doctor JoseMiguelMantas: director JulioOrtega, director JoseACarrillo{};
+doctor JoseLuisGarrido: director MiguelGea{};
+doctor CeciliaNegrete: director JoseSamos, director ManuelTorres{};
+doctor SergioDamas: director OscarCordon{};
+doctor PedroCano: director JuanCarlosTorres{};
+doctor MarioRossainz: director ManuelCapel{};
+
+doctor MiguelLastra: director CarlosUrena {};
+doctorando RubenGarcia: dirige CarlosUrena {};
+doctorando RosanaMontes: dirige CarlosUrena {};
+
+doctor GermanArroyo: director FranciscoVelasco, director DomingoMartin {};
+
+#include "IngSoft.C"
